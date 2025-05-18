910227f524866a3950aa51c40cd9c780d8e8ab11
charu <charugupta860@gmail.com>
2025-05-19 01:54:40 +0530

    Updated Routes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
-const jwt_Token = process.env.JWT_SECRET; // move to .env in production
+const jwt_Token = process.env.JWT_SECRET; 
 
 function authMiddleware(req, res, next) {
-  const token = req.headers.authorization; // Bearer <token>
+  const token = req.headers.authorization; 
   if (!token) return res.status(401).json({ error: "Access denied" });
 
   try {
